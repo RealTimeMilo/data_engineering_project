0880226a05c0207d5c833329ed72deca39340eca
“RealTimeMilo” <miloxrr@gmail.com>
2026-06-03 16:34:21 -0500

    new dag

---
+++ dags/p2.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from airflow.operators.bash import BashOperator
+
+from airflow import DAG
+
+with DAG(
+    "current_build",
+    description="Print the current build loaded to the worker",
+    schedule="0 * * * *",
+    start_date=datetime(2022, 6, 10),
+    default_args={
+        "owner": "Data Platform Orchestration",
+    },
+    catchup=True,
+) as dag:
+
+    t1 = BashOperator(
+        task_id="print_current_build",
+        bash_command="ls -la /opt/airflow/",
+    )
